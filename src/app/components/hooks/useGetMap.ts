fc60f8b201f9ef61043405eb48a5f71bcd752b8d
student <daisychepkirui@gmail.com>
2024-10-02 20:13:39 +0300

    dashboard added files

---
+++ src/app/components/hooks/useGetMap.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import { fetchSensorData } from "@/app/utils/utils";
+export const useSensorData = () => {
+  const [sensorData, setSensorData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null); // Ensure error is of type string or null
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await fetchSensorData();
+        setSensorData(data);
+      } catch (err) {
+        // Type guard to check if the error is an instance of Error
+        if (err instanceof Error) {
+          setError(err.message); // Access the message safely
+        } else {
+          setError("An unknown error occurred"); // Fallback for unknown error types
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, []);
+  return { sensorData, loading, error };
+};
